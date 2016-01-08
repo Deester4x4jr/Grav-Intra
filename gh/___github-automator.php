57d141ab1cac3341cf1a0eea128167ce77ba03fd
Josh DeSherlia <josh@desherlia.net>
2016-01-07 22:11:42 -0500

    Updating GitHub Automator to be very Verbose

--- gh/___github-automator.php
+++ gh/___github-automator.php
@@ -1,26 +1,43 @@
 <?php
-date_default_timezone_set('America/Los_Angeles');
-ignore_user_abort(true);
-set_time_limit(0);
 
-$repo          = '~/grav-intra';
-$branch        = 'master';
-$output        = array();
+	date_default_timezone_set('America/Los_Angeles');
+	ignore_user_abort(true);
+	set_time_limit(0);
 
-// update github Repo
-$results['Navigating to ' . $repo] = shell_exec('cd ' . $repo);
-$results['Fetching ' . $branch . ' from origin'] = shell_exec('git fetch origin ' . $branch);
-$results['Resetting to origin/' . $branch] = shell_exec('git reset --hard origin/' . $branch);
-$results['Running Garbage Collection'] = shell_exec('git gc');
+	$logFile		= rtrim(getcwd(), '/').'/___github-log.txt';
+	$repo 			= '~/grav-intra';
+	$branch			= 'master';
+	$cmds			= array(
+						'Navigating to ' . $repo => 'cd ' . $repo,
+						'Fetching ' . $branch . ' from origin' => 'git fetch origin ' . $branch,
+						'Resetting to origin/' . $branch => 'git reset --hard origin/' . $branch,
+						'Running Garbage Collection' => 'git gc' . $branch,
+					);
 
-// build logs
-$output[] = "\n" . 'Begin Sync with Upstream - ' . date('Y-m-d, H:i:s', time()) . "\n";
-foreach ($result as $k=>$v) {
-	if (empty($v) {
-		$v = "No news is good news!  Success!!";
+	// start logging
+	file_put_contents($logFile,
+		"\n" . '==============================================' . "\n" .
+		'Init Sync with Upstream -- ' . date('Y-m-d, H:i:s', time()) . "\n" . 
+		'----------------------------------------------' . "\n\n",
+		FILE_APPEND);
+
+	// update github Repo
+	foreach ($cmds as $k=>$v) {
+		
+		$result = shell_exec($v);
+		
+		if (empty($result)) {
+			$result = "\n" . $k . ' .......... Success!!';
+		} else {
+			$result = "\n\n" . $k . ' .......... Success:' . "\n" . $result;
+		}
+
+		file_put_contents($logFile, $result, FILE_APPEND);
 	}
-	$output[] = $k"\n============================\n" . $v;
-}
 
-// redirect output to logs
-file_put_contents(rtrim(getcwd(), '/').'/___github-log.txt', implode("\n", $output) . "\n-- End Sync with Upstream --\n", FILE_APPEND);
+	// end logging
+	file_put_contents($logFile,
+		"\n\n" . '----------------------------------------------' . "\n" .
+		'Stop Sync with Upstream -- ' . date('Y-m-d, H:i:s', time()) . "\n" .
+		'==============================================' . "\n",
+		FILE_APPEND);
